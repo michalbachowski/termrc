2f3656072c3083c4d245ed4925b6c5453b110fa1
Michał Bachowski <michal@bachowski.pl>
2014-08-28 18:42:13 +0200

    Remove 'test' directory

--- init.sh
+++ init.sh
@@ -5,16 +5,16 @@ mkdir "backup";
 for dotfile in `ls -1 dotfiles/`; do
     echo "Installing $dotfile";
 
-    if [ -e ~/test/".$dotfile" ]; then
+    if [ -e ~/".$dotfile" ]; then
         echo "    Backing up $dotfile into backup/$dotfile";
-        mv ~/test/.$dotfile ./backup/
-    elif [ -h ~/test/".$dotfile" ]; then
-        echo "    Removing symlink ~/test/.$dotfile"
-        rm ~/test/.$dotfile
+        mv ~/.$dotfile ./backup/
+    elif [ -h ~/".$dotfile" ]; then
+        echo "    Removing symlink ~/.$dotfile"
+        rm ~/.$dotfile
     fi
 
-    echo "    Creating symlink ~/test/.$dotfile";
-    ln -s ./dotfiles/$dotfile ~/test/.$dotfile
+    echo "    Creating symlink ~/.$dotfile";
+    ln -s ./dotfiles/$dotfile ~/.$dotfile
 
     echo "   Done.";
 done;
