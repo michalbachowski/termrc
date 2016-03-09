5cfd7dffe2ae9ea65f02df6c00b7ec8605433cb8
Michal Bachowski <mbachows@akamai.com>
2016-03-09 10:02:15 +0100

    Update init.sh
    Instead of moving symlink, copy it's destination, and remove link.
    Also copy "dotfile" to backup directory under "non-dot" name

--- init.sh
+++ init.sh
@@ -3,11 +3,12 @@
 mkdir "backup";
 
 for dotfile in `ls -1 dotfiles/`; do
-    echo "Installing $dotfile";
+    echo "Installing $dotfile"
 
     if [ -e ~/".$dotfile" ]; then
-        echo "    Backing up $dotfile into backup/$dotfile";
-        mv ~/.$dotfile ./backup/
+        echo "    Backing up $dotfile into backup/$dotfile"
+        cp $(readlink ~/.$dotfile) ./backup/$dotfile
+        rm ~/.$dotfile
     elif [ -h ~/".$dotfile" ]; then
         echo "    Removing symlink ~/.$dotfile"
         rm ~/.$dotfile
@@ -16,11 +17,11 @@ for dotfile in `ls -1 dotfiles/`; do
     echo "    Creating symlink ~/.$dotfile";
     ln -s `pwd`/dotfiles/$dotfile ~/.$dotfile
 
-    echo "   Done.";
-done;
+    echo "   Done."
+done
 
 # new version of resty
 echo "Initializing ~/.restyrc with current version"
 curl -L http://github.com/micha/resty/raw/master/resty > ~/.restyrc
 
-echo "Done";
+echo "Done"
